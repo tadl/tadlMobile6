715ac150bfa22f25b4183b2eb0aa724f7c479aa4
William Rockwood <wrockwood@tadl.org>
2023-01-24 21:20:34 +0000

    android build
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'org.tadl.tadl',
+  appId: 'org.TADL.TADLMobile',
   appName: 'TADL',
   webDir: 'www',
   bundledWebRuntime: false
