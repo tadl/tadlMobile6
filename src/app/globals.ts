460088015ee452f19a2d4d27091e55d9f9cbca5c
William Rockwood <wrockwood@tadl.org>
2024-09-12 17:11:45 +0000

    fix eresource links
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -18,8 +18,8 @@ export class Globals {
   /* CUSTOMIZABLE VARIABLES */
 
   /* app version */
-  public app_version: string = '6.3.0';
-  public update_version: string = '2024081500';
+  public app_version: string = '6.3.1';
+  public update_version: string = '2024091200';
 
   public device_info: any;
   public system_color: any = window.matchMedia("(prefers-color-scheme: dark)");
