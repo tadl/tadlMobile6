6e5a8286b6960fbb521ab592d9b922034914ef00
William Rockwood <wrockwood@tadl.org>
2025-08-18 17:13:17 -0400

    native fixes for android statusbar
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- android/app/src/main/java/org/TADL/TADLMobile/MainActivity.java
+++ android/app/src/main/java/org/TADL/TADLMobile/MainActivity.java
@@ -1,5 +1,14 @@
 package org.TADL.TADLMobile;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import androidx.core.view.WindowCompat;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    // Do NOT draw behind the status bar; inset the content instead
+    WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
+  }
+}
