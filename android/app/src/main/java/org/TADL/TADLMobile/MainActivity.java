4fa61d72b302d029f4750455a1db0fcb6de8e964
William Rockwood <wrockwood@tadl.org>
2025-08-19 14:49:15 -0400

    minimal mainactivity.java
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- android/app/src/main/java/org/TADL/TADLMobile/MainActivity.java
+++ android/app/src/main/java/org/TADL/TADLMobile/MainActivity.java
@@ -1,39 +1,14 @@
 package org.TADL.TADLMobile;
 
 import android.os.Bundle;
-import android.view.View;
-
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
-    WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
-
-    final View content = findViewById(android.R.id.content);
-    if (content != null) {
-      ViewCompat.setOnApplyWindowInsetsListener(content, (v, insets) -> {
-        Insets topInsets = insets.getInsets(WindowInsetsCompat.Type.statusBars());
-        Insets bottomInsets = insets.getInsets(WindowInsetsCompat.Type.navigationBars());
-
-        v.setPadding(
-            v.getPaddingLeft(),
-            topInsets.top,
-            v.getPaddingRight(),
-            bottomInsets.bottom
-        );
-
-        return insets;
-      });
-
-      ViewCompat.requestApplyInsets(content);
-    }
+    // Let Android/Capacitor inset the content. No manual padding.
+    WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
   }
 }
