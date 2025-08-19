608795f17f3822c1d3239fce28207bf9ec26248d
William Rockwood <wrockwood@tadl.org>
2025-08-19 10:56:14 -0400

    bigger hammer
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- android/app/src/main/java/org/TADL/TADLMobile/MainActivity.java
+++ android/app/src/main/java/org/TADL/TADLMobile/MainActivity.java
@@ -1,14 +1,29 @@
 package org.TADL.TADLMobile;
 
+import android.os.Build;
 import android.os.Bundle;
-import com.getcapacitor.BridgeActivity;
+import android.view.View;
+import android.view.WindowManager;
+
 import androidx.core.view.WindowCompat;
+
+import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    // Do NOT draw behind the status bar; inset the content instead
+
     WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
+
+    getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+    if (Build.VERSION.SDK_INT >= 21) {
+      getWindow().setStatusBarColor(0xFFFFFFFF);
+    }
+
+    View webView = getBridge() != null ? getBridge().getWebView() : null;
+    if (webView != null) {
+      webView.setFitsSystemWindows(true);
+    }
   }
 }
